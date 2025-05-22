bc61b1011f2ebf5a49075c4913a746340b034c22
Denis Lüthi <denis.luethi@students.fhnw.ch>
2025-05-22 16:07:48 +0200

    Anpassen GUI
    Karte nun als HTML

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ from streamlit_folium import st_folium
 from Karte import map_with_layers
 import os
 
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 # Streamlit GUI
 st.title('Baugesuch Viewer')
-st.write('Zeigt eine Übersicht der Baugesuche in der Umgebung an.')
+left_column, right_column = st.columns([4,1])
+left_column.write('Zeigt eine Übersicht der Baugesuche in der Umgebung an.')
 
-st_folium(map_with_layers, width=1000, height=800)
+right_column.button("Neues Baugesuch erfassen", key="new_application")
+
+
+with open("data/map.html", "r", encoding="utf-8") as f:
+    html_data = f.read()
+# In Streamlit anzeigen
+st.components.v1.html(html_data, height=600, width=1300)
